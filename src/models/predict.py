784f612a7a2df78dae6a58be4463884712d8dc63
Jefferson Kauan Cavalcante Chaves <jefferson.chaves@ilos.com.br>
2025-12-09 10:03:48 -0300

    Atualizando readme

--- src/models/predict.py
+++ src/models/predict.py
@@ -2,23 +2,33 @@
 import joblib
 import numpy as np
 from tensorflow.keras.models import load_model
+from soja_model import PositionalEmbedding   
 
+
+# carregamento dos scalers
 scaler_X = joblib.load("artifacts/scaler_X.joblib")
 y_scalers = joblib.load("artifacts/scaler_y.joblib")
-model = load_model("artifacts/soja_model.keras")
 
-X_new = np.zeros((1, scaler_X.n_features_in_))  # exemplo zeros
+# carregamento do modelo com custom_objects
+model = load_model(
+    "artifacts/soja_model.keras",
+    custom_objects={"PositionalEmbedding": PositionalEmbedding}
+)
+
+# exemplo de entrada
+X_new = np.zeros((1, scaler_X.n_features_in_))
 X_new_scaled = scaler_X.transform(X_new)
 
+# predição
 preds = model.predict(X_new_scaled)
 
 ton = preds[0].ravel()
-kg = preds[1].ravel()
-val = preds[2].ravel()
+#kg = preds[1].ravel()
+val = preds[1].ravel()
 
-ton_r = y_scalers["Toneladas"].inverse_transform(ton.reshape(-1,1)).ravel()
-
-val_r = y_scalers["Valor_USD_FOB"].inverse_transform(val.reshape(-1,1)).ravel()
+# inversões
+ton_r = y_scalers["Toneladas"].inverse_transform(ton.reshape(-1, 1)).ravel()
+val_r = y_scalers["Valor_USD_FOB"].inverse_transform(val.reshape(-1, 1)).ravel()
 
 print("Predição (unscaled):")
 print({"Toneladas": ton_r[0], "Valor_USD_FOB": val_r[0]})
